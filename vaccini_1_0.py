f524744d61426c0ce3ffbc6104d6c7bc0a6fa68e
davidemanzilli <61005304+davidemanzilli@users.noreply.github.com>
2021-03-28 18:35:58 +0200

    Add files via upload
    grafico a barre CONSEGNE-SOMMINISTRAZIONI VACCINI

--- vaccini_1_0.py
+++ vaccini_1_0.py
@@ -2,11 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("dati_vaccini.csv")
+data = pd.read_csv("dati_vaccini.csv") #apro il file da leggere
 
-data2 = data[["dosi_somministrate", "nome_area"]]
+data2 = data[["dosi_somministrate", "nome_area", "dosi_consegnate"]] #creo un dataframe con i dati che mi servono 
 
-print (data2)
+# print (data2) #vedo a schermo il dataframe 
 
-data2.plot(kind="bar", x="nome_area", y="dosi_somministrate", figsize=(15, 8))
-plt.show()
+data2.plot(kind="bar", x="nome_area", figsize=(16, 8)) #creo il grafico 
+plt.title("CONSEGNE-SOMMINISTRAZIONI")
+plt.yticks([100000*k for k in range(0,31)])
+plt.ylabel("INTERVALLO 0-3 MILIONI")
+plt.xlabel("REGIONI")
+plt.savefig("CONSEGNE-SOMMINISTRAZIONI.png")
+plt.show() #mostro il grafico 
